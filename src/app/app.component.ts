e248b0f92119e50ff859526aee3833cc5a549c45
Shehrozkhannn <sharozekkhan@gmail.com>
2025-02-08 19:19:05 +0500

    Implemented the UI for the tasks and lists section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TaskViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
