ab870d8c7e47afeeaa3f4951df82ab19e5faf96d
Shehrozkhannn <sharozekkhan@gmail.com>
2025-05-18 19:49:38 +0500

    create login page and did its functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListPopupComponent } from './pages/new-list-popup/new-list-popup.component';
 import { NewTaskPopupComponent } from './pages/new-task-popup/new-task-popup.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/lists', pathMatch: 'full' },
@@ -9,5 +10,6 @@ export const routes: Routes = [
     { path: 'lists', component: TaskViewComponent },
     { path: 'lists/:listId', component: TaskViewComponent },
     { path: 'lists/:listId/new-task', component: NewTaskPopupComponent },
+    { path: 'login', component: LoginPageComponent },
   ];
 
