837a7c346a569b511d8d722bc3342214e64d96a1
Shehrozkhannn <sharozekkhan@gmail.com>
2025-07-08 01:14:39 +0500

    working on login authentication

---
+++ src/app/auth.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { WebRequestService } from './web-request.service';
+import { Router } from '@angular/router';
+import { shareReplay, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient, private webService: WebRequestService, private router:Router) { }
+
+  login(email:string, password: string){
+    return this.webService.login(email,password).pipe(
+      shareReplay(),
+      tap((res:any)=>{
+       this.setSession(res.body._id, res.headers.get('x-access-token'), res.headers.get('x-refresh-token'));
+       console.log('logged in')
+      })
+    )
+  }
+
+  logout(){
+    this.removeSession();
+  }
+
+  private setSession(userId: string, accessToken: string, refreshToken: string){
+    localStorage.setItem('user-id',userId);
+    localStorage.setItem('access-token',accessToken);
+    localStorage.setItem('refresh-token',refreshToken);
+  }
+
+  private removeSession(){
+    localStorage.removeItem('user-id');
+    localStorage.removeItem('access-token');
+    localStorage.removeItem('refresh-token');
+  }
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  constructor(private authService: AuthService){
+
+  }
+
+  login(email:string,password:string){
+    this.authService.login(email,password).subscribe((res)=>{
+      console.log(res)
+    })
+  }
 }
